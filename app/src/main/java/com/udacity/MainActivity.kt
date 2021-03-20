6034273c4b6dfa8d141860f4c85fd5a65547a8e1
Chris Scheid <justmobiledev1@gmail.com>
2021-03-20 05:58:35 -0400

    add project

---
+++ app/src/main/java/com/udacity/MainActivity.kt
@@ -0,0 +1,104 @@
+package com.udacity
+
+import android.app.DownloadManager
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.net.Uri
+import android.os.Bundle
+import android.view.View
+import android.widget.RadioButton
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+
+enum class SelectedDownloadType {
+    TYPE_GLIDE,
+    TYPE_UDACITY,
+    TYPE_RETROFIT
+}
+
+class MainActivity : AppCompatActivity() {
+
+    private var downloadID: Long = 0
+
+    private lateinit var notificationManager: NotificationManager
+    private lateinit var pendingIntent: PendingIntent
+    private lateinit var action: NotificationCompat.Action
+    private var selectedDownloadType = SelectedDownloadType.TYPE_GLIDE
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        registerReceiver(receiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
+
+        custom_button.setOnClickListener {
+            when(selectedDownloadType) {
+                SelectedDownloadType.TYPE_GLIDE -> download(GLIDE_URL)
+                SelectedDownloadType.TYPE_UDACITY -> download(UDACITY_URL)
+                SelectedDownloadType.TYPE_RETROFIT -> download(RETROFIT_URL)
+            }
+        }
+    }
+
+    fun onRadioButtonClicked(view: View) {
+        if (view is RadioButton) {
+            // Is the button now checked?
+            val checked = view.isChecked
+
+            // Check which radio button was clicked
+            when (view.getId()) {
+                R.id.radio_button_glide ->
+                    if (checked) {
+                        selectedDownloadType = SelectedDownloadType.TYPE_GLIDE
+                    }
+                R.id.radio_button_loadapp ->
+                    if (checked) {
+                        selectedDownloadType = SelectedDownloadType.TYPE_UDACITY
+                    }
+                R.id.radio_button_retrofit ->
+                    if (checked) {
+                        selectedDownloadType = SelectedDownloadType.TYPE_RETROFIT
+                    }
+            }
+        }
+    }
+
+    private val receiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            val id = intent?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
+        }
+    }
+
+    private fun download(url: String) {
+        val request =
+            DownloadManager.Request(Uri.parse(url))
+                .setTitle(getString(R.string.app_name))
+                .setDescription(getString(R.string.app_description))
+                .setRequiresCharging(false)
+                .setAllowedOverMetered(true)
+                .setAllowedOverRoaming(true)
+
+        val downloadManager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
+        downloadID =
+            downloadManager.enqueue(request)// enqueue puts the download request in the queue.
+    }
+
+    companion object {
+        // URL definitions
+        private const val GLIDE_URL = "https://github.com/bumptech/glide/archive/refs/heads/master.zip"
+        private const val UDACITY_URL =
+            "https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter/archive/master.zip"
+        private const val RETROFIT_URL = "https://github.com/square/retrofit/archive/refs/heads/master.zip"
+
+        // Notification
+        private const val CHANNEL_ID = "channelId"
+    }
+
+}
